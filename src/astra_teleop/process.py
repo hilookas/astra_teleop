e86f60b23d3414c174cfa62c1966d46aea24f14d
lookas <i@18kas.com>
2024-10-25 20:34:51 +0800

    Add robicu support

--- src/astra_teleop/process.py
+++ src/astra_teleop/process.py
@@ -38,10 +38,11 @@ def rvec_tvec_from_transform(transform):
     return rvec, tvec
 
 def get_detect():
-    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
     aruco_detection_parameters = cv2.aruco.DetectorParameters()
     # aruco_detection_parameters.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX
     aruco_detection_parameters.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_APRILTAG
+    aruco_detection_parameters.aprilTagQuadDecimate = 2
     # aruco_detection_parameters.cornerRefinementWinSize = 2
     detector = cv2.aruco.ArucoDetector(aruco_dict, aruco_detection_parameters)
     
@@ -67,30 +68,29 @@ def get_solve(scale=1):
     # set coordinate system
     # Coordinate setting: 
     # https://stackoverflow.com/questions/53277597/fundamental-understanding-of-tvecs-rvecs-in-opencv-aruco
-    marker_length_mm = 0.056 * scale
-    obj_points = np.array([
-        (-marker_length_mm / 2, marker_length_mm / 2, 0), # top left
-        (marker_length_mm / 2, marker_length_mm / 2, 0), # top right
-        (marker_length_mm / 2, -marker_length_mm / 2, 0), # bottom right
-        (-marker_length_mm / 2, -marker_length_mm / 2, 0), # bottom left
-    ])
-
-    marker_z_to_center = -0.075/2 * scale
-    marker_rotation_matrix = {
-        # right gripper
-        239: pr.matrix_from_euler([-math.pi/2, -math.pi/2, 0], 2, 1, 0, False), # original: right_bottom, now: front
-        241: pr.matrix_from_euler([-math.pi/2, 0, 0], 2, 1, 0, False), # original: right_front, now: top
-        242: pr.matrix_from_euler([-math.pi/2, math.pi/2, 0], 2, 0, 1, False), # original: left_side, now: left
-        243: pr.matrix_from_euler([-math.pi/2, -math.pi/2, 0], 2, 0, 1, False), # original: right_side, now: right
-        229: pr.matrix_from_euler([math.pi/2, math.pi/2, 0], 2, 1, 0, False), # original: right_top, now: back
-        # left gripper
-        233: pr.matrix_from_euler([-math.pi/2, -math.pi/2, 0], 2, 1, 0, False), # original: right_bottom, now: front
-        235: pr.matrix_from_euler([-math.pi/2, 0, 0], 2, 1, 0, False), # original: right_front, now: top
-        236: pr.matrix_from_euler([-math.pi/2, math.pi/2, 0], 2, 0, 1, False), # original: left_side, now: left
-        237: pr.matrix_from_euler([-math.pi/2, -math.pi/2, 0], 2, 0, 1, False), # original: right_side, now: right
-        231: pr.matrix_from_euler([math.pi/2, math.pi/2, 0], 2, 1, 0, False), # original: right_top, now: back
+    obj_points_map = {
+        # top_left, top_right, bottom_right, bottom_left
+        0:  [ (-15.00, -15.00,  48.28), (-15.00,  15.00,  48.28), ( 15.00,  15.00,  48.28), ( 15.00, -15.00,  48.28) ],
+        
+        1:  [ ( 23.54, -15.00,  44.75), ( 23.54,  15.00,  44.75), ( 44.75,  15.00,  23.54), ( 44.75, -15.00,  23.54) ],
+        2:  [ (-15.00, -23.54,  44.75), ( 15.00, -23.54,  44.75), ( 15.00, -44.75,  23.54), (-15.00, -44.75,  23.54) ],
+        3:  [ (-23.54,  15.00,  44.75), (-23.54, -15.00,  44.75), (-44.75, -15.00,  23.54), (-44.75,  15.00,  23.54) ],
+        4:  [ ( 15.00,  23.54,  44.75), (-15.00,  23.54,  44.75), (-15.00,  44.75,  23.54), ( 15.00,  44.75,  23.54) ],
+        
+        5:  [ ( 48.28, -15.00,  15.00), ( 48.28,  15.00,  15.00), ( 48.28,  15.00, -15.00), ( 48.28, -15.00, -15.00) ],
+        6:  [ ( 23.54, -44.75,  15.00), ( 44.75, -23.54,  15.00), ( 44.75, -23.54, -15.00), ( 23.54, -44.75, -15.00) ],
+        7:  [ (-15.00, -48.28,  15.00), ( 15.00, -48.28,  15.00), ( 15.00, -48.28, -15.00), (-15.00, -48.28, -15.00) ],
+        8:  [ (-44.75, -23.54,  15.00), (-23.54, -44.75,  15.00), (-23.54, -44.75, -15.00), (-44.75, -23.54, -15.00) ],
+        9:  [ (-48.28,  15.00,  15.00), (-48.28, -15.00,  15.00), (-48.28, -15.00, -15.00), (-48.28,  15.00, -15.00) ],
+        10: [ (-23.54,  44.75,  15.00), (-44.75,  23.54,  15.00), (-44.75,  23.54, -15.00), (-23.54,  44.75, -15.00) ],
+        11: [ ( 15.00,  48.28,  15.00), (-15.00,  48.28,  15.00), (-15.00,  48.28, -15.00), ( 15.00,  48.28, -15.00) ],
+        12: [ ( 44.75,  23.54,  15.00), ( 23.54,  44.75,  15.00), ( 23.54,  44.75, -15.00), ( 44.75,  23.54, -15.00) ],
+        
+        13: [ ( 44.75, -15.00, -23.54), ( 44.75,  15.00, -23.54), ( 23.54,  15.00, -44.75), ( 23.54, -15.00, -44.75) ],
+        14: [ (-15.00, -44.75, -23.54), ( 15.00, -44.75, -23.54), ( 15.00, -23.54, -44.75), (-15.00, -23.54, -44.75) ],
+        15: [ (-44.75,  15.00, -23.54), (-44.75, -15.00, -23.54), (-23.54, -15.00, -44.75), (-23.54,  15.00, -44.75) ],
+        16: [ ( 15.00,  44.75, -23.54), (-15.00,  44.75, -23.54), (-15.00,  23.54, -44.75), ( 15.00,  23.54, -44.75) ],
     }
-    left_hand_markers = [ 233, 235, 236, 237, 231 ]
     
     def solve(
         camera_matrix, distortion_coefficients,
@@ -98,141 +98,89 @@ def get_solve(scale=1):
         left_hand_cb, right_hand_cb,
         debug=False,
         debug_image=None,
-        mitigate_projection_ambiguity=True,
-        err_diff_thres_start=0.1,
-        err_diff_thres_end=0.3,
+        min_aruco_thres=4
     ):
         camera_matrix = np.array(camera_matrix, dtype=np.float32)
         distortion_coefficients = np.array(distortion_coefficients, dtype=np.float32)
-        aruco_corners = np.array(aruco_corners, dtype=np.float32)
-        aruco_ids = np.array(aruco_ids)
-
-        right_max_area = 0
-        right_tag2cam_in_use = None
-        left_max_area = 0
-        left_tag2cam_in_use = None
-
-        # Calculate pose for each marker
-        # not None or [[]] see: https://stackoverflow.com/questions/11295609/how-can-i-check-whether-a-numpy-array-is-empty-or-not
-        if aruco_ids.ndim and aruco_ids.size:
-            for aruco_corner, aruco_id in zip(aruco_corners, aruco_ids):
-                aruco_id = aruco_id.item()
-                is_left_hand = aruco_id in left_hand_markers
-                
-                if mitigate_projection_ambiguity:
-                    # There is a projection ambiguity problem
-                    # See https://github.com/opencv/opencv/issues/8813 for more detail
-                    num, (rvec1, rvec2), (tvec1, tvec2), reprojection_errors = cv2.solvePnPGeneric(
-                        obj_points,
-                        aruco_corner,
-                        camera_matrix, distortion_coefficients,
-                        flags=cv2.SOLVEPNP_IPPE_SQUARE
-                    )
-
-                    # if debug:
-                    #     cv2.drawFrameAxes(
-                    #         debug_image,
-                    #         camera_matrix, distortion_coefficients,
-                    #         rvec1, tvec1,
-                    #         marker_length_mm * 1, 2
-                    #     )
-                    #     cv2.drawFrameAxes(
-                    #         debug_image,
-                    #         camera_matrix, distortion_coefficients,
-                    #         rvec2, tvec2,
-                    #         marker_length_mm * 1, 2
-                    #     )
-
-                    tag2cam1 = transform_from_rvec_tvec(rvec1.squeeze(), tvec1.squeeze())
-                    tag2cam2 = transform_from_rvec_tvec(rvec2.squeeze(), tvec2.squeeze())
-
-                    reprojection_errors = reprojection_errors.squeeze()
-                    err_diff = reprojection_errors[1] - reprojection_errors[0]
-                    if err_diff < err_diff_thres_start:
-                        err_coff = [0.5, 0.5]
-                        print("ambiguity", err_coff)
-                    elif err_diff < err_diff_thres_end:
-                        membership = (err_diff - err_diff_thres_start) / (err_diff_thres_end - err_diff_thres_start)
-                        err_coff = [0.5 + membership * 0.5, 0.5 - membership * 0.5]
-                        print("ambiguity", err_coff)
-                    else:
-                        err_coff = [1, 0]
-                    if debug:
-                        print(err_diff)
-                        print(err_coff)
-
-                    tag2cam = pt.transform_from_pq(pt.pq_slerp(
-                        pt.pq_from_transform(tag2cam1),
-                        pt.pq_from_transform(tag2cam2),
-                        err_coff[1]
-                    ))
-                else:
-                    # rvec shape (3, 1)
-                    # tvec shape (3, 1)
-                    unknown_variable, rvec, tvec = cv2.solvePnP(
-                        obj_points, # shape: (4, 3) # point coord in 3d space
-                        aruco_corner, # shape: (1, 4, 2) # point coord in camera 2d space
-                        camera_matrix, distortion_coefficients
-                    )
-                    
-                    tag2cam = transform_from_rvec_tvec(rvec.squeeze(), tvec.squeeze())
-                
-                # tag_transforms.append((aruco_id, tag2cam))
-
-                # move to center
-                tag2cam = pt.concat(pt.transform_from_pq([0, 0, marker_z_to_center, 1, 0, 0, 0]), tag2cam)
-
-                # rotate to right pose
-                if aruco_id in marker_rotation_matrix:
-                    tag2cam = pt.concat(pt.transform_from(marker_rotation_matrix[aruco_id], [0, 0, 0]), tag2cam)
-
-                # use the marker have largest area size
-                area = cv2.contourArea(aruco_corner)
-                if is_left_hand:
-                    if area > left_max_area:
-                        left_max_area = area
-                        left_tag2cam_in_use = tag2cam
-                else:
-                    if area > right_max_area:
-                        right_max_area = area
-                        # right_tag2cam_in_use = (tag2cam, tag2cam1, tag2cam2)
-                        right_tag2cam_in_use = tag2cam
+        
+        if aruco_ids is None:
+            aruco_ids = np.array([])
+            aruco_corners = np.array([])
             
-            if right_tag2cam_in_use is not None:
-                if right_hand_cb is not None:
-                    # right_hand_cb(*right_tag2cam_in_use)
-                    right_hand_cb(right_tag2cam_in_use)
+        left_tags = []
+        right_tags = []
+        
+        for aruco_id, aruco_corner in zip(aruco_ids, aruco_corners):
+            aruco_id = aruco_id.item()
+            aruco_corner = aruco_corner.squeeze() # shape: (4, 2)
+            is_left_hand = aruco_id >= 18 # 18~34 is left hand
+            
+            if is_left_hand:
+                left_tags.append((aruco_id, aruco_corner))
+            else:
+                right_tags.append((aruco_id, aruco_corner))
+                
+        right_tags.sort(key=lambda tag: cv2.contourArea(tag[1]), reverse=True)
+        if len(right_tags) < min_aruco_thres:
+            print(f"detected less than {min_aruco_thres} aruco tags")
+            return
+        right_tags = right_tags[:min_aruco_thres] # pick biggest four tag
+        # print(right_tags[0][0])
+        
+        obj_points = []
+        img_points = []
+        
+        for aruco_id, aruco_corner in right_tags:
+            if aruco_id in obj_points_map:
+                img_points.extend(aruco_corner)
+                obj_points.extend(obj_points_map[aruco_id])
 
-                if debug:
-                    rvec, tvec = rvec_tvec_from_transform(right_tag2cam_in_use)
-                    cv2.drawFrameAxes(
-                        debug_image,
-                        camera_matrix, distortion_coefficients,
-                        rvec, tvec,
-                        marker_length_mm * 1, 2
-                    )
+        # rvec shape (3, 1)
+        # tvec shape (3, 1)
+        unknown_variable, rvec, tvec = cv2.solvePnP(
+            np.array(obj_points), # shape: (4 * n, 3) # point coord in 3d space
+            np.array(img_points), # shape: (4 * n, 2) # point coord in camera 2d space
+            camera_matrix, distortion_coefficients
+        )
+        
+        tag2cam = transform_from_rvec_tvec(rvec.squeeze(), tvec.squeeze())
+        
+        right_tag2cam = None
+        left_tag2cam = None
 
-            if left_tag2cam_in_use is not None:
-                if left_hand_cb is not None:
-                    left_hand_cb(left_tag2cam_in_use)
+        right_tag2cam = tag2cam
+ 
+        if right_tag2cam is not None:
+            if right_hand_cb is not None:
+                right_hand_cb(right_tag2cam)
 
-                if debug:
-                    rvec, tvec = rvec_tvec_from_transform(left_tag2cam_in_use)
-                    cv2.drawFrameAxes(
-                        debug_image,
-                        camera_matrix, distortion_coefficients,
-                        rvec, tvec,
-                        marker_length_mm * 1, 2
-                    )
+            if debug:
+                rvec, tvec = rvec_tvec_from_transform(right_tag2cam)
+                cv2.drawFrameAxes(
+                    debug_image,
+                    camera_matrix, distortion_coefficients,
+                    rvec, tvec,
+                    50/2, 2
+                )
+
+        if left_tag2cam is not None:
+            if left_hand_cb is not None:
+                left_hand_cb(left_tag2cam)
+
+            if debug:
+                rvec, tvec = rvec_tvec_from_transform(left_tag2cam)
+                cv2.drawFrameAxes(
+                    debug_image,
+                    camera_matrix, distortion_coefficients,
+                    rvec, tvec,
+                    50/2, 2
+                )
     return solve
 
 def process(
     device="/dev/video0", calibration_directory="./calibration_images", 
     left_hand_cb=None, right_hand_cb=None,
     debug=False,
-    mitigate_projection_ambiguity=True,
-    err_diff_thres_start=0.1,
-    err_diff_thres_end=0.3,
 ):
     # Open camera
     cam = open_cam(device)
@@ -262,9 +210,6 @@ def process(
             left_hand_cb, right_hand_cb,
             debug,
             debug_image,
-            mitigate_projection_ambiguity,
-            err_diff_thres_start,
-            err_diff_thres_end,
         )
 
         if debug:
@@ -282,6 +227,7 @@ def process(
             # plt.draw()
             # plt.pause(0.001)
 
+            # debug_image = cv2.flip(debug_image, 1)
             cv2.imshow('Debug Image', debug_image)
             # debug_image2 = cv2.undistort(debug_image, camera_matrix, distortion_coefficients)
             # cv2.imshow('Debug Image 2', debug_image2)
