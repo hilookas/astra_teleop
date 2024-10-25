76aa190d1dd9e97ddc7f046b3eaa0b97058a4c6e
lookas <i@18kas.com>
2024-10-25 14:22:38 +0000

    Speedup detection

--- src/astra_teleop/process.py
+++ src/astra_teleop/process.py
@@ -42,6 +42,7 @@ def get_detect():
     aruco_detection_parameters = cv2.aruco.DetectorParameters()
     # aruco_detection_parameters.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX
     aruco_detection_parameters.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_APRILTAG
+    aruco_detection_parameters.aprilTagQuadDecimate = 2
     # aruco_detection_parameters.cornerRefinementWinSize = 2
     detector = cv2.aruco.ArucoDetector(aruco_dict, aruco_detection_parameters)
     
